1c10c3c1c74bd44ace073aab40a5f20d1911a022
Chen Qun <mary84060117@gmail.com>
2022-06-12 16:14:33 +0800

    button change againagain

--- screens/Add_Expenditure_1.js
+++ screens/Add_Expenditure_1.js
@@ -13,24 +13,69 @@ import { supabase } from '../lib/supabase';
 import { Input } from 'react-native-elements';
 import { useNavigation } from '@react-navigation/native';
 import Catebutton from '../components/Catebutton.js';
-import Cateunbutton from '../components/Catebutton.js';
+import SimpleSelectButton from 'react-native-simple-select-button';
 
 export default Add_Expenditure_1 = () => {
-    [pressstatus1, SetPressstatus1] = useState(false)
-    const press = (text) => {
-        SetPressstatus1 = !pressstatus1;
-        return Alert.alert(text)
-
-    }
+    
     return (
         <SafeAreaView style={{ flexDirection : 'column' }}>
             <View style={{ flexDirection: 'row' }}>
-                {pressstatus1 ? <Catebutton text='Expenditure' onPress={press('this is expenditure')} />
-                    : <Cateunbutton text='Expenditure' onPress={press('this is spam')} />}
-           
-            <Catebutton text='Income' onPress={() => Alert.alert('this is income')} />
-            <Catebutton text='Lending' onPress={() => Alert.alert('this is lending')} />
-                <Catebutton text='Borrowing' onPress={() => Alert.alert('this is borrowing')} />
+                <SimpleSelectButton
+                    text="Expenditure"
+                    textSize={14}
+                    iconName="checkcircleo"
+                    iconColor="#fff"
+                    iconSize={14}
+                    buttonDefaultColor="yellow"
+                    buttonSelectedColor="orange"
+                    textDefaultColor="#333"
+                    textSelectedColor="#fff"
+                    isChecked={true}
+                    onPress={() => Alert.alert('this is expenditure')}
+                    style={{marginVertical: 20}}
+                />
+              
+                <SimpleSelectButton
+                    text="Income"
+                    textSize={14}
+                    iconName="checkcircleo"
+                    iconColor="#fff"
+                    iconSize={14}
+                    buttonDefaultColor="yellow"
+                    buttonSelectedColor="orange"
+                    textDefaultColor="#333"
+                    textSelectedColor="#fff"
+                    isChecked={true}
+                    onPress={() => Alert.alert('this is income')}
+                    style={{ marginVertical: 20 }}
+                /><SimpleSelectButton
+                    text="Lending"
+                    textSize={14}
+                    iconName="checkcircleo"
+                    iconColor="#fff"
+                    iconSize={14}
+                    buttonDefaultColor="yellow"
+                    buttonSelectedColor="orange"
+                    textDefaultColor="#333"
+                    textSelectedColor="#fff"
+                    isChecked={true}
+                    onPress={() => Alert.alert('this is lending')}
+                    style={{ marginVertical: 20 }}
+                /><SimpleSelectButton
+                    text="Borrowing"
+                    textSize={14}
+                    iconName="checkcircleo"
+                    iconColor="#fff"
+                    iconSize={14}
+                    buttonDefaultColor="yellow"
+                    buttonSelectedColor="orange"
+                    textDefaultColor="#333"
+                    textSelectedColor="#fff"
+                    isChecked={true}
+                    onPress={() => Alert.alert('this is borrowing')}
+                    style={{ marginVertical: 20 }}
+                />
+            
             </View>
             <View>
                 <Text>haha</Text>
