1f74692b2deef64da94ea205518dc28db2b11172
Jingyi <jingyi0830@gmail.com>
2022-08-04 11:24:57 +0800

    app.js updated

--- App.js
+++ App.js
@@ -1,19 +1,8 @@
-import { StatusBar } from 'expo-status-bar';
-import { Alert, Button, Image, StyleSheet, SafeAreaView, Text, View } from 'react-native';
-import Flatbutton from './components/Flatbutton.js';
-import Login_page from './screens/Login_page.js'
-import { NavigationContainer, useNavigation } from '@react-navigation/native';
-import { createNativeStackNavigator }from '@react-navigation/native-stack';
+import { StyleSheet } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
 import  AuthStacks  from './navigation/AuthStacks.js';
 import Home_navigation  from './navigation/Home_navigation.js';
-import { useState, useEffect, useContext } from 'react';
 import { authentication } from './lib/firebase.js';
-
-
-
-
-
-const Stack = createNativeStackNavigator();
 
 
 export default App = ({ navigation }) => {
@@ -22,28 +11,3 @@ export default App = ({ navigation }) => {
            {authentication.currentUser !== null ? <Home_navigation /> : <AuthStacks />}
         </NavigationContainer>)
 }
-//{auth ? <Home_navigation/> : <AuthStacks/>}
-//<Home_navigation />
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        toppadding: 10,
-        backgroundColor: '#EBECF0',
-        alignItems: 'center',
-       justifyContent: 'flex-start'
-    }, image: {
-        width: 500,
-        height: 800,
-        justifyContent: "flex-start"
-    }, fixToText: {
-        flexDirection: 'row',
-        justifyContent: "space-around"
-    }, separator: {
-        marginVertical: 8,
-        orderBottomColor: 'white',
-        borderBottomWidth: StyleSheet.hairlineWidth,
-
-    },
-});
-
